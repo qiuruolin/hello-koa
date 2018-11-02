bca301afb640275c53c0b933fef5a5dd43cee609
qiuruolin <1312492221@qq.com>
2018-11-02 11:15:29 +0800

    添加koa-bodyparser处理post请求的数据

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 //添加koa-router处理url
 const router = require('koa-router')()
+//添加koa-bodyparser用来处理post的数据
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
+
+app.use(bodyParser())
 
 app.use(async(ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
@@ -15,7 +19,25 @@ router.get('/hello/:name', async(ctx, next) => {
 })
 
 router.get('/', async(ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>'
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+})
+
+router.post('/signin', async(ctx, next) => {
+    const name = ctx.request.body.name || ''
+    const password = ctx.request.body.password || ''
+    console.log(`signin with name: ${name}, password: ${password}`)
+    if(name === 'koa' && password === '12345'){
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`
+    }
+    else{
+        ctx.response.body = `<h1>Login failed</h1>
+        <p><a href="/">Try Again</a></p>`
+    }
 })
 
 app.use(router.routes())
