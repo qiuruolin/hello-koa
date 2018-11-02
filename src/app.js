9aca61ce3cf9a50747c3656edf794e63aeec5982
qiuruolin <1312492221@qq.com>
2018-11-02 11:06:46 +0800

    添加koa-router处理url

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 const Koa = require('koa')
+//添加koa-router处理url
+const router = require('koa-router')()
 
 const app = new Koa()
 
 app.use(async(ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
     await next()
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
 })
+
+router.get('/hello/:name', async(ctx, next) => {
+    const name = ctx.params.name
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`
+})
+
+router.get('/', async(ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>'
+})
+
+app.use(router.routes())
 
 app.listen(3000)
 console.log('app started at port 3000...')
